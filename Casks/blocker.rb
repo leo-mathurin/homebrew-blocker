95db479d7812e88016838a15cd21324ff7975059
Léo Mathurin <lelemathrin69@gmail.com>
2026-04-30 17:09:19 +0200

    chore: switch to universal binary

--- Casks/blocker.rb
+++ Casks/blocker.rb
@@ -1,11 +1,8 @@
 cask "blocker" do
-  arch arm: "arm64", intel: "x64"
-
   version "0.1.0"
-  sha256 arm:   "492445638b74ebca680e3cf3982bd37c663028cbc03685dcc2151b91f46865f1",
-         intel: "50c7711a94ff4f1611f12ce92a534fb2ee400b652e279c7282b4b01f07a7fcab"
+  sha256 "492445638b74ebca680e3cf3982bd37c663028cbc03685dcc2151b91f46865f1"
 
-  url "https://github.com/leo-mathurin/blocker-releases/releases/download/v#{version}/blocker-#{version}-#{arch}.dmg",
+  url "https://github.com/leo-mathurin/blocker-releases/releases/download/v#{version}/blocker-#{version}.dmg",
       verified: "github.com/leo-mathurin/blocker-releases/"
   name "Blocker"
   desc "Minimal website blocker with recurring schedules"
