b881f0174f6c9346c9c8b7d3743d3c0fb6211c6a
Léo Mathurin <leo.mathurin@epitech.eu>
2026-04-30 12:39:10 +0200

    fix: add verified parameter (homepage and url domains differ)

--- Casks/blocker.rb
+++ Casks/blocker.rb
@@ -5,7 +5,8 @@ cask "blocker" do
   sha256 arm:   "492445638b74ebca680e3cf3982bd37c663028cbc03685dcc2151b91f46865f1",
          intel: "50c7711a94ff4f1611f12ce92a534fb2ee400b652e279c7282b4b01f07a7fcab"
 
-  url "https://github.com/leo-mathurin/blocker-releases/releases/download/v#{version}/blocker-#{version}-#{arch}.dmg"
+  url "https://github.com/leo-mathurin/blocker-releases/releases/download/v#{version}/blocker-#{version}-#{arch}.dmg",
+      verified: "github.com/leo-mathurin/blocker-releases/"
   name "Blocker"
   desc "Minimal website blocker with recurring schedules"
   homepage "https://getblocker.app"
